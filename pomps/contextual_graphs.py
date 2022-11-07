4650eaf2d7f048e8cc3cf6f11e230d1af42f120f
varsenyantitan <varsenyantitan>
2022-11-07 14:28:21 +0400

    Initial version of simplification (Proposition 5)

--- pomps/contextual_graphs.py
+++ pomps/contextual_graphs.py
@@ -5,42 +5,71 @@ from pomps.utils import union
 
 class ContextualCausalGraph(nx.DiGraph):
 
-    def __init__(self, edges=None, uc_variables: tp.Union[tp.Set[str], str] = None):
-        # interventional_variables: tp.Set[str], contextual_variables: tp.Set[str],
-        #          latent_variables: tp.Set[str],
-        # self.interventional_variables = interventional_variables
-        # self.contextual_variables = contextual_variables
-        # self.latent_variables = latent_variables
-        # assert len(self.latent_variables & (self.interventional_variables | self.contextual_variables)) == 0, \
-        #     "Latent variable can not be contextual or interventional"
+    def __init__(self, interventional_variables: tp.Set[str] = None,
+                 contextual_variables: tp.Set[str] = None, edges=None,
+                 uc_variables: tp.Union[tp.Set[str], str] = None, target='Y'):
+        self.interventional_variables = interventional_variables
+        self.contextual_variables = contextual_variables
+        self.target = target
         super().__init__(edges)
         if uc_variables is None:
             uc_variables = set()
         if uc_variables == 'auto':
             uc_variables = {node for node in self.nodes if node.startswith("U")}
         self.uc_variables = uc_variables
+        self.validate_ucs()
+
+    def validate_ucs(self):
+        removable = []
+        for uc in self.uc_variables:
+            confounded = self.children(uc)
+            if len(confounded) == 1:
+                removable.append(uc)
+            if len(confounded) > 2:
+                raise ValueError("Single UC confounds more than two variables consider projecting to multiple UCs")
+        self.shrink_uc_variables(set(removable))
 
     def shrink_uc_variables(self, uc_variables: tp.Set[str]):
         assert uc_variables.issubset(self.uc_variables)
         self.remove_nodes_from(self.uc_variables - uc_variables)
         self.uc_variables = self.uc_variables - uc_variables
 
-    def parents(self, target: tp.Union[str, tp.Set[str]]):
+    def parents(self, target: tp.Union[str, tp.Set[str]], include_uc=False):
         if isinstance(target, str):
             target = {target}
-        return union([set(self.predecessors(t)) for t in target])
+        if include_uc:
+            return union([set(self.predecessors(t)) for t in target])
+        else:
+            return union([set(self.predecessors(t)) for t in target]) - self.uc_variables
+
+    def children(self, target: tp.Union[str, tp.Set[str]]):
+        if isinstance(target, str):
+            target = {target}
+        return union([set(self.successors(t)) for t in target])
 
     def remove_incoming_edges(self, target: tp.Union[str, tp.Set[str]]) -> "ContextualCausalGraph":
         if isinstance(target, str):
             target = {target}
-        copy = ContextualCausalGraph(self.edges, self.uc_variables)
-        uc_variables = self.uc_variables
+        copy = ContextualCausalGraph(edges=self, uc_variables=self.uc_variables,
+                                     interventional_variables=self.interventional_variables,
+                                     contextual_variables=self.contextual_variables)
         for t in target:
-            parents = copy.parents(t)
-            uc_variables = uc_variables - parents
+            parents = copy.parents(t, include_uc=True)
             edges = [(p, t) for p in parents]
             copy.remove_edges_from(edges)
-        copy.shrink_uc_variables(uc_variables)
+        copy.validate_ucs()
+        return copy
+
+    def remove_outgoing_edges(self, target: tp.Union[str, tp.Set[str]]) -> "ContextualCausalGraph":
+        if isinstance(target, str):
+            target = {target}
+        copy = ContextualCausalGraph(edges=self, uc_variables=self.uc_variables,
+                                     interventional_variables=self.interventional_variables,
+                                     contextual_variables=self.contextual_variables)
+        for t in target:
+            children = copy.children(t)
+            edges = [(t, c) for c in children]
+            copy.remove_edges_from(edges)
         return copy
 
     def topological_order(self, include_uc=False):
--- pomps/controllers.py
+++ pomps/controllers.py
@@ -1,6 +1,6 @@
 from pomps.policy_scope import MixedPolicyScope
 from pomps.contextual_graphs import ContextualCausalGraph
-from networkx import is_directed_acyclic_graph
+from networkx import is_directed_acyclic_graph, descendants
 
 
 class MPSDAGController:
@@ -12,3 +12,19 @@ class MPSDAGController:
         mut_graph.add_edges_from(mps.pairs)
         return mut_graph, is_directed_acyclic_graph(mut_graph)
 
+    @classmethod
+    def simplify(cls, graph: ContextualCausalGraph):
+        new_interventional = []
+        for inter in graph.interventional_variables:
+            related = descendants(graph, inter) & (graph.contextual_variables | {graph.target})
+            if len(related) != 0:
+                new_interventional.append(inter)
+        new_interventional = set(new_interventional)
+        graph_related_reduced = graph.remove_outgoing_edges(new_interventional)
+        redundant = descendants(graph_related_reduced, graph_related_reduced.target) & new_interventional
+        z_redundant = descendants(graph.remove_incoming_edges(redundant),  graph.target)
+        new_context = graph.contextual_variables - redundant - z_redundant
+
+        return ContextualCausalGraph(edges=graph.subgraph(set(graph.nodes) - z_redundant),
+                                     interventional_variables=new_interventional,
+                                     contextual_variables=new_context)
