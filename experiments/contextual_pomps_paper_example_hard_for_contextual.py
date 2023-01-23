62ada8d37c889b087c8b7fde9be46bd98844dd89
varsenyantitan <varsenyantitan>
2023-01-24 01:04:07 +0400

    Fixed HEBO issue with CaBO

--- experiments/contextual_pomps_paper_example_hard_for_contextual.py
+++ experiments/contextual_pomps_paper_example_hard_for_contextual.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
-from experiments.scms import pomps_example_hard_for_contextual as environ
+from experiments.scms import pomps_example_hard_for_contextual as environ, n_inter, n_context
 import datetime
 import torch
 from experiments.pomps_experiment import CoBOExperiment, MixedPolicyScope
@@ -20,10 +20,8 @@ if __name__ == "__main__":
     torch.manual_seed(seed)
     experiment_name = "contextual_pomps_paper_graph0_hard_for_contextual"
 
-    exp = CoBOExperiment(environ.fcm, {"X1", "X2", "X3", "X4", "X5", "X6", "X7", 'X8', "X9", "X10", "X11", "X12"},
-                         {"C", "C2", 'C3', 'C0', "C4", 'Ct0', 'Ct1', 'Ct2', 'Ct3', 'Ct4',
-                          'Ctt0', 'Ctt1', 'Ctt2', 'Ctt3', 'Ctt4', 'Cttt0', 'Cttt1', 'Cttt2', 'Cttt3', 'Cttt4',
-                          'Ctttt0', 'Ctttt1', 'Ctttt2', 'Ctttt3', 'Ctttt4'},
+    exp = CoBOExperiment(environ.fcm, set(["X1", "X2"] + [f"Xt{i}" for i in range(n_inter)]),
+                          set(["C1", "C0", "C2", "C3", "C4"] + [f"Ct{i}" for i in range(n_context)]),
                          environ.domain, "Y", [MixedPolicyScope(set())], debug=smoke_test,
                          n_iter=16 if smoke_test else n_iter, experiment_name=experiment_name)
 
